a2c484f1cfd472e167cf9e480050217d84922f08
Githaiga05 <ggithaiga379@gmail.com>
2024-03-20 03:56:56 -0400

    Python attributes

---
+++ Gabrielspython/Exception handling.py
@@ -0,0 +1,29 @@
+#The try block will generate an error, because x is not defined:
+
+try:
+  print(x)
+except:
+  print("An exception occurred")
+
+
+  # The try block does not raise any errors, so the else block is executed:
+try:
+  print("Hello")
+except:
+ print("Something went wrong")
+else:
+  print("Nothing went wrong")
+
+
+#The finally block gets executed no matter if the try block raises any errors or not:
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
+
+
+
+
+
--- Gabrielspython/Functions.py
+++ Gabrielspython/Functions.py
@@ -1,5 +1,47 @@
-def addition(x,y): #x and y are parameters
-    return x+y #return is the result of the operation
+#finding largest number in a list
+def my_function(*number):
+    print("largest number is " , max(number))
 
-#calling a function
-print("The sum of 5 and 9 is: ",addition(9, 5))
+my_function(10,20,9)
+
+
+#adds items in a tuple
+'''
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange, berry")
+thistuple = tuple(y)
+
+print(thistuple)
+'''
+
+#deletes lists of keys in a dictionary
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+
+#repeating values in an element
+my_list = ["apple", "banana", "banana", "apple", "cherry"]
+repeated_elements = []
+
+for item in my_list:
+    if my_list.count(item)>1:
+        if item not in repeated_elements:
+          repeated_elements.append(item)
+
+
+print(repeated_elements)
+
+
+#converting lists to dictionary
+list1 = ["model", "year", "brand"]
+list2 = ["harrier", "2020", "Toyota"]
+print("My list1",list1)
+print("My list2",list2)
+mydict=dict(zip(list1,list2))
+print("My dict",mydict)
